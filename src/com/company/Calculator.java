97a1969a3dce28e3404ee94a3d23a87bbb54c60f
SpaceAccordeonist <Tesla257>
2020-09-05 14:50:41 +0300

    initial commit

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.util.Random;
+
+public class Calculator {
+    public static int sumArrayFor(int[] array){
+        int result = 0;
+        for(int num: array){
+            result += num;
+        }
+        return result;
+    }
+    public static int sumArrayWhile(int[] array){
+        int result = 0;
+        int i = 0;
+        while(i < array.length){
+            result += array[i];
+            i++;
+        }
+        return result;
+    }
+    public static int sumArrayDo(int[] array){
+        int result = 0;
+        int i = -1;
+        do{
+            result += array[++i];
+        }while (i < array.length - 1);
+        return result;
+    }
+
+    public static double[] getHarmonicSeries(int count){
+        double[] result = new double[count];
+        for(int i = 0; i < count; i++){
+            result[i] = 1f/(i+1f);
+        }
+        return result;
+    }
+
+    enum RandomType{
+        MATH, RANDOM
+    }
+    public static double[] getRandomArray(int count, RandomType type){
+        double[] result = new double[count];
+        switch (type) {
+            case RANDOM:
+                for (int i = 0; i < count; i++) {
+                    result[i] = new Random().nextDouble();
+                }
+                break;
+            case MATH:
+                for (int i = 0; i < count; i++) {
+                    result[i] = Math.random();
+                }
+                break;
+        }
+        return result;
+    }
+
+    public static int getFactorial(int number){
+        int result = 1;
+        for(int i = 2; i <= number; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.printf("program #1\nfor: %d\n", Calculator.sumArrayFor(arr));
+        System.out.printf("do-while: %d\n", Calculator.sumArrayDo(arr));
+        System.out.printf("while: %d\n", Calculator.sumArrayWhile(arr));
+        System.out.println();
+
+        System.out.println("program #2");
+        for(String arg: args){
+            System.out.println(arg);
+        }
+        System.out.println();
+
+        System.out.println("program #3");
+        double[] series = Calculator.getHarmonicSeries(10);
+        for(double num: series){
+            System.out.printf("%.2f ", num);
+        }
+        System.out.println("\n");
+
+        System.out.println("program #4");
+        System.out.println("Math.random() :");
+        double[] rand = Calculator.getRandomArray(10, Calculator.RandomType.MATH);
+        for(double num: rand){
+            System.out.printf("%.2f ", num);
+        }
+        System.out.println("\nnew Random().nextDouble() :");
+        rand = Calculator.getRandomArray(10, Calculator.RandomType.RANDOM);
+        for(double num: rand){
+            System.out.printf("%.2f ", num);
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("program #5");
+        System.out.printf("factorial of %d = %d",5, Calculator.getFactorial(5));
+
+    }
+}
